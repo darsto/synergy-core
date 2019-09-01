9ad3dda20298849bde8b3c5da87e9b5fe463de0e
Dariusz Stojaczyk <darek.stojaczyk@gmail.com>
2019-09-01 21:48:14 +0200

    Add C API to inject input events
    Add syn_* family of functions to simulate input on
    a remote system. Synergys will also spawn a new thread
    capable of calling those functions.
    
    The goal is to have another shared library preloaded
    with synergys. The library would only contain the code
    to send proper keypresses / mouse clicks or occasionally
    to wait for the real user input.
    
    Signed-off-by: Dariusz Stojaczyk <darek.stojaczyk@gmail.com>

--- src/lib/server/ClientProxy1_1.cpp
+++ src/lib/server/ClientProxy1_1.cpp
@@ -19,9 +19,85 @@
 #include "server/ClientProxy1_1.h"
 
 #include "synergy/ProtocolUtil.h"
+#include "arch/Arch.h"
 #include "base/Log.h"
 
+#include "c_api.h"
+
 #include <cstring>
+#include <unistd.h>
+
+static synergy::IStream *g_stream = NULL;
+static synergy_c_ops *g_ops;
+static int g_mouse_x = 1280;
+static int g_mouse_y = 562;
+static bool g_run = true;;
+
+void
+syn_press_key(int key, int mask, int button)
+{
+    ProtocolUtil::writef(g_stream, kMsgDKeyDown, key, mask, button);
+}
+
+void
+syn_unpress_key(int key, int mask, int button)
+{
+    ProtocolUtil::writef(g_stream, kMsgDKeyUp, key, mask, button);
+}
+
+void
+syn_enter_screen(void)
+{
+    ProtocolUtil::writef(g_stream, kMsgCEnter, g_mouse_x, g_mouse_y, 25, 8912);
+}
+
+void
+syn_leave_screen(void)
+{
+    ProtocolUtil::writef(g_stream, kMsgCLeave);
+}
+
+void
+syn_get_mouse(int *x, int *y)
+{
+    *x = g_mouse_x;
+    *y = g_mouse_y;
+}
+
+void
+syn_click_mouse(int button)
+{
+    ProtocolUtil::writef(g_stream, kMsgDMouseDown, button);
+}
+
+void
+syn_unclick_mouse(int button)
+{
+    ProtocolUtil::writef(g_stream, kMsgDMouseUp, button);
+}
+
+
+void
+syn_mouse_move(int x, int y)
+{
+    if (x == g_mouse_x && y == g_mouse_y) {
+        return;
+    }
+
+    ProtocolUtil::writef(g_stream, kMsgDMouseMove, x, y);
+    g_mouse_x = x;
+    g_mouse_y = y;
+}
+
+static void *
+bgthread_main(void *)
+{
+    while (g_run) {
+        g_ops->bgthread_cb();
+    }
+
+    return NULL;
+}
 
 //
 // ClientProxy1_1
@@ -30,18 +106,29 @@
 ClientProxy1_1::ClientProxy1_1(const String& name, synergy::IStream* stream, IEventQueue* events) :
     ClientProxy1_0(name, stream, events)
 {
-    // do nothing
+    g_stream = stream;
+    ARCH->newThread(bgthread_main, NULL);
 }
 
 ClientProxy1_1::~ClientProxy1_1()
 {
-    // do nothing
+    if (g_ops) {
+        g_ops->exit_cb();
+        g_run = false;
+        usleep(1000);
+    }
 }
 
 void
 ClientProxy1_1::keyDown(KeyID key, KeyModifierMask mask, KeyButton button)
 {
     LOG((CLOG_DEBUG1 "send key down to \"%s\" id=%d, mask=0x%04x, button=0x%04x", getName().c_str(), key, mask, button));
+
+    if (g_ops) {
+        g_ops->keypress_cb(key, mask, button);
+    }
+
+
     ProtocolUtil::writef(getStream(), kMsgDKeyDown, key, mask, button);
 }
 
@@ -63,6 +150,9 @@ ClientProxy1_1::keyUp(KeyID key, KeyModifierMask mask, KeyButton button)
 void
 ClientProxy1_1::mouseMove(SInt32 xAbs, SInt32 yAbs)
 {
+    g_mouse_x = xAbs;
+    g_mouse_y = yAbs;
+
     LOG((CLOG_DEBUG2 "send mouse move to \"%s\" %d,%d", getName().c_str(), xAbs, yAbs));
     ProtocolUtil::writef(getStream(), kMsgDMouseMove, xAbs, yAbs);
 }
@@ -71,6 +161,12 @@ void
 ClientProxy1_1::enter(SInt32 xAbs, SInt32 yAbs,
                 UInt32 seqNum, KeyModifierMask mask, bool)
 {
+    g_mouse_x = xAbs;
+    g_mouse_y = yAbs;
+    if (g_ops) {
+        g_ops->change_focus_cb(true);
+    }
+
     ProtocolUtil::writef(getStream(), kMsgCEnter,
                                 xAbs, yAbs, seqNum, mask);
 }
@@ -78,6 +174,10 @@ ClientProxy1_1::enter(SInt32 xAbs, SInt32 yAbs,
 bool
 ClientProxy1_1::leave()
 {
+    if (g_ops) {
+        g_ops->change_focus_cb(false);
+    }
+
     ProtocolUtil::writef(getStream(), kMsgCLeave);
     return true;
 }
